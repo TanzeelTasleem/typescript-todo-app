b3d75df239994f84814b8e41e62f45679f80c41a
TanzeelTasleem <tanzeeltasleem648@gmail.com>
2020-07-24 18:09:39 +0500

    todo app updated

--- src/components/todoItem.tsx
+++ src/components/todoItem.tsx
@@ -3,7 +3,6 @@ import { Todo } from "../Types/type";
 import { GlobalContext } from "../context/globalContext";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import ListItemText from "@material-ui/core/ListItemText";
 import IconButton from "@material-ui/core/IconButton";
